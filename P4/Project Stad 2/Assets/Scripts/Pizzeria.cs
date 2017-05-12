bb37aaf3c7aec20345e31a383fcb09ace9f39928
gmd1apeterbos <97040824@st.deltion.nl>
2017-05-12 11:19:25 +0200

    Very basic house finding

---
+++ P4/Project Stad 2/Assets/Scripts/Pizzeria.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pizzeria : MonoBehaviour {
+    public List<GameObject> houses = new List<GameObject>();
+    public GameObject targetHouse;
+	// Use this for initialization
+	void Start () {
+        findHouses();
+        ChooseHouse();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(targetHouse.name);
+	}
+
+    public void ChooseHouse()
+    {
+        int i = Random.Range(0, houses.Count);
+        targetHouse = houses[i];
+    }
+
+    void findHouses()
+    {
+        GameObject[] homes = GameObject.FindGameObjectsWithTag("House");
+        foreach(GameObject g in homes)
+        {
+            houses.Add(g);
+        }
+    }
+}
