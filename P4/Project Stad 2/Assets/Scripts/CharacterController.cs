2cfe7127fcf552f299c7cb7d2de3b29f461cdad1
gmd1apeterbos <97040824@st.deltion.nl>
2017-05-12 11:01:33 +0200

    Temp shit

---
+++ P4/Project Stad 2/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    public float moveSpeed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetButton("w"))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("a"))
+        {
+            transform.Rotate(0, -90, 0);
+        }
+
+        if (Input.GetButton("s"))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("d"))
+        {
+            transform.Rotate(0, +90, 0);
+        }
+    }
+}
