6a36bd8673252747fbfd25ae15a56fe8c1ff37fd
Dominik Beens <dominik.beens@hotmail.nl>
2017-05-12 10:28:28 +0200

    timer

--- P4/Project Stad 2/Assets/Scripts/GameManager.cs
+++ P4/Project Stad 2/Assets/Scripts/GameManager.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public UIManager uim;
+
+    public bool timerOn;
+    public float timeToCountDown = 180f;
+
+    private void Start()
+    {
+        uim = GameObject.FindWithTag("UIM").GetComponent<UIManager>();
+    }
+
+    private void Update()
+    {
+        if (timerOn)
+        {
+            timeToCountDown -= Time.deltaTime;
+        }
+    }
 }
