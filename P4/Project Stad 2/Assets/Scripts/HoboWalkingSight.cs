9e7900e2b6080f9b2283984f355a0a9fd8f4a38e
Dominik Beens <dominik.beens@hotmail.nl>
2017-05-23 09:04:44 +0200

    commit

--- P4/Project Stad 2/Assets/Scripts/HoboWalkingSight.cs
+++ P4/Project Stad 2/Assets/Scripts/HoboWalkingSight.cs
@@ -6,27 +6,38 @@ public class HoboWalkingSight : MonoBehaviour
 {
 
     private HoboWalking hoboWalking;
-    public LayerMask lm;
+    public GameObject player;
 
     private void Start()
     {
         hoboWalking = GetComponentInParent<HoboWalking>();
+        player = GameObject.FindWithTag("Player");
     }
 
-    public void OnTriggerStay(Collider other)
+    private void Update()
     {
-        if (other.tag == "Player")
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 toPlayer = player.transform.position - hoboWalking.transform.position;
+        if (Vector3.Dot(forward, toPlayer) > 0)
         {
-            RaycastHit hit;
-            if (Physics.Raycast(hoboWalking.transform.position, hoboWalking.transform.position - other.transform.position, out hit, Vector3.Distance(hoboWalking.transform.position, other.transform.position), lm))     //dit doet hij ergens helemaal anders naartoe.. na 30 min geef ik het op
-            {
-                Debug.DrawRay(hoboWalking.transform.position, -hoboWalking.transform.position - -other.transform.position, new Color (0,0,0));
-                if (hit.transform.gameObject.tag == "Player")
-                {
-                    print("chase");
-                    hoboWalking.ChasePlayer();
-                }
-            }
+            print("The other transform is infront of me!");
         }
     }
+
+    //public void OnTriggerStay(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        RaycastHit hit;
+    //        if (Physics.Raycast(hoboWalking.transform.position, other.transform.position - hoboWalking.transform.position, out hit, Vector3.Distance(hoboWalking.transform.position, other.transform.position)))     //dit doet hij ergens helemaal anders naartoe.. na 30 min geef ik het op
+    //        {
+    //            Debug.DrawRay(hoboWalking.transform.position, other.transform.position - hoboWalking.transform.position, new Color (0,0,0));
+    //            if (hit.transform.gameObject.tag == "Player")
+    //            {
+    //                print("chase");
+    //                hoboWalking.ChasePlayer();
+    //            }
+    //        }
+    //    }
+    //}
 }
