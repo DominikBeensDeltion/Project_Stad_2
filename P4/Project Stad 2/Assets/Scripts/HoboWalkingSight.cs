864bd04feb7ac9046ba83be15841d2f97670067d
Dominik Beens <dominik.beens@hotmail.nl>
2017-05-23 10:08:13 +0200

    hobo sight

--- P4/Project Stad 2/Assets/Scripts/HoboWalkingSight.cs
+++ P4/Project Stad 2/Assets/Scripts/HoboWalkingSight.cs
@@ -6,7 +6,10 @@ public class HoboWalkingSight : MonoBehaviour
 {
 
     private HoboWalking hoboWalking;
-    public GameObject player;
+    private GameObject player;
+
+    public float sightRange = 10;
+    public float sightAngle = 30;
 
     private void Start()
     {
@@ -18,9 +21,31 @@ public class HoboWalkingSight : MonoBehaviour
     {
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 toPlayer = player.transform.position - hoboWalking.transform.position;
-        if (Vector3.Dot(forward, toPlayer) > 0)
+
+        if (Vector3.Distance(hoboWalking.transform.position, player.transform.position) <= sightRange)
         {
-            print("The other transform is infront of me!");
+            print("player is in range");
+
+            if (Vector3.Dot(forward, toPlayer) > 0)
+            {
+                print("player is infront of hobo");
+
+                Vector3 targetDir = player.transform.position - hoboWalking.transform.position;
+                float angle = Vector3.Angle(targetDir, transform.forward);
+
+                //Debug.DrawRay(hoboWalking.transform.position, hoboWalking.transform.position + new Vector3((300), 0), Color.red);
+                //Debug.DrawRay(hoboWalking.transform.position, hoboWalking.transform.position + new Vector3((-300), 0), Color.blue);
+
+
+                if (angle < sightAngle)
+                {
+                    print("player is in sight");
+
+                    //Debug.DrawRay(hoboWalking.transform.position, player.transform.position - hoboWalking.transform.position, Color.yellow);
+
+                    hoboWalking.ChasePlayer();
+                }
+            }
         }
     }
 
