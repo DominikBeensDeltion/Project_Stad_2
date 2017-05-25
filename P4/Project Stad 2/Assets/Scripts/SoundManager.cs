7f83388741e94aaa81b6cb74975c5bb8d2b6ba43
gmd1apeterbos <97040824@st.deltion.nl>
2017-05-25 14:19:14 +0200

    We got a volume slider now

---
+++ P4/Project Stad 2/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour {
+    public Slider volumeSlider;
+	// Use this for initialization
+	void Start () {
+        volumeSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ValueChangeCheck()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+}
