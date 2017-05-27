03efd732044789a084b02ca227fb92158d5225f5
gmd1apeterbos <97040824@st.deltion.nl>
2017-05-27 15:34:15 +0200

    I hate these pickups

--- P4/Project Stad 2/Assets/Scripts/Pickup.cs
+++ P4/Project Stad 2/Assets/Scripts/Pickup.cs
@@ -11,6 +11,7 @@ public class Pickup : MonoBehaviour
     public bool giveTime;
     public bool giveQual;
     public bool giveSpeed;
+    public bool pickedUp;
 
     public float timeToGive;
     public float speedToGive;
@@ -37,6 +38,7 @@ public class Pickup : MonoBehaviour
     {
         if (col.gameObject.tag == "Player" || col.gameObject.tag == "PlayerCar")
         {
+            pickedUp = true;
             AudioSource.PlayClipAtPoint(ding, transform.position);
             Destroy(gameObject);
         }
@@ -44,23 +46,31 @@ public class Pickup : MonoBehaviour
 
     void OnDestroy()
     {
-        if (giveTime)
+        if (pickedUp)
         {
-            gm.timeToCountDown += timeToGive;
-        }
-        else if (giveQual)
-        {
-            PizzaQuality.quality += QualToGive;
-        }
-        //Not sure about this
-        //Error = Coroutine could not be started because the game object PickupSpeed(Clone) is inactive!
-        //ook al start ik de coroutine in de gamemanager
+                if (giveTime)
+                {
+                    gm.timeToCountDown += timeToGive;
+                    pickedUp = false;
+                }
+                else if (giveQual)
+                {
+                    PizzaQuality.quality += QualToGive;
+                    pickedUp = false;
+                }
+            //Not sure about this
+            //Error = Coroutine could not be started because the game object PickupSpeed(Clone) is inactive!
+            //ook al start ik de coroutine in de gamemanager
 
-        //else if (giveSpeed)
-        //{
-        //    cr.moveSpeed += speedToGive;
-        //    StartCoroutine(gm.RemoveSpeed());
-        //}
+            //else if (giveSpeed)
+            //{
+            //    cr.moveSpeed += speedToGive;
+            //    StartCoroutine(gm.RemoveSpeed());
+            //    pickedUp = false;
+            //}
+
+        }
         GameManager.currentPickups -= 1;
+        
     }
 }
