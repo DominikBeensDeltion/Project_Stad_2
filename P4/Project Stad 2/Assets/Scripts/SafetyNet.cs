736ba87c0c708bb74d522917a4769087dbd0f154
gmd1apeterbos <97040824@st.deltion.nl>
2017-06-02 22:42:06 +0200

    Safety "Net" in case you fuck up

---
+++ P4/Project Stad 2/Assets/Scripts/SafetyNet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafetyNet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collided");
+        Vector3 up = col.transform.position;
+        up.y = col.transform.position.y + 39;
+        col.transform.position = up;
+    }
+}
