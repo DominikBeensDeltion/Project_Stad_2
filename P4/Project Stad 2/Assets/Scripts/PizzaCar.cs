52273a534cff175270569edab4d268ff861fbff2
Dominik Beens <dominik.beens@hotmail.nl>
2017-06-05 17:06:08 +0200

    fixed car raycast, player steps out at driver seat

--- P4/Project Stad 2/Assets/Scripts/PizzaCar.cs
+++ P4/Project Stad 2/Assets/Scripts/PizzaCar.cs
@@ -20,10 +20,13 @@ public class PizzaCar : MonoBehaviour
     public float forwardSpeed;
     public float backwardSpeed;
 
+    public float gravity = 1;
+
     public AudioClip engine;
     public AudioSource sound;
 
     public Rigidbody rb;
+
 
     void Start()
     {
@@ -33,6 +36,9 @@ public class PizzaCar : MonoBehaviour
 
     void Update()
     {
+        //ray for checking if the player can get out of the car
+        //Debug.DrawRay(new Vector3(carObject.transform.position.x, carObject.transform.position.y + 0.5f, carObject.transform.position.z), transform.TransformDirection(Vector3.left), Color.red);
+
         if (inCar)
         {
             if (canGetOut)
@@ -42,7 +48,6 @@ public class PizzaCar : MonoBehaviour
                     GetOut();
                 }
             }
-
         }
     }
 
@@ -96,9 +101,12 @@ public class PizzaCar : MonoBehaviour
                 sound.Play();
             }
 
-            //float horizontalMovement = (Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed);
-            //float verticalMovement = (Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
+            //working player-like movement
+            //float horizontalMovement = (Input.GetAxis("Horizontal") * Time.deltaTime * currentMoveSpeed);
+            //float verticalMovement = (Input.GetAxis("Vertical") * Time.deltaTime * currentMoveSpeed);
             //carObject.transform.position = new Vector3(transform.position.x + horizontalMovement, 0, transform.position.z + verticalMovement);
+
+            //rb.velocity = new Vector3(horizontalMovement, -gravity, verticalMovement);
 
             //Vector3 direction = new Vector3(horizontalMovement, 0, verticalMovement);
 
@@ -113,11 +121,10 @@ public class PizzaCar : MonoBehaviour
     void GetOut()
     {
         RaycastHit hit;
-        if (Physics.Raycast(carObject.transform.position, Vector3.left, out hit, 2))
+        if (Physics.Raycast(new Vector3(carObject.transform.position.x, carObject.transform.position.y + 0.5f, carObject.transform.position.z), transform.TransformDirection(Vector3.left), out hit, 2))
         {
             //U suck ray
         }
-
         else
         {
             player.SetActive(true);
@@ -125,14 +132,12 @@ public class PizzaCar : MonoBehaviour
             {
                 Destroy(playerClone);
             }
-            inCar = false;
+            player.transform.position = carObject.transform.position - (transform.right * 2);
             player.transform.SetParent(null);
-            player.transform.position = new Vector3(carObject.transform.position.x - 3, carObject.transform.position.y, carObject.transform.position.z);
+            inCar = false;
             canGetOut = false;
             sound.Stop();
-
         }
-        Debug.DrawRay(carObject.transform.position, Vector3.left, Color.red);
     }
 
     IEnumerator GetOutCoolDown()
