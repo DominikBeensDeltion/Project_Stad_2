d0b423060e5789ce26b2e820cb2294da4e3bddd8
Dominik Beens <dominik.beens@hotmail.nl>
2017-06-08 13:02:27 +0200

    fixed name gen, added road texture

--- P4/Project Stad 2/Assets/Scripts/RandomNameGen.cs
+++ P4/Project Stad 2/Assets/Scripts/RandomNameGen.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomNameGen : MonoBehaviour {
+public class RandomNameGen : MonoBehaviour
+{
+
     public List<string> prefix = new List<string>();
     public List<string> middle = new List<string>();
     public List<string> suffix = new List<string>();
 
-    public string randomName;
-
+    private string randomName;
 
     public string Name()
     {
         int one = Random.Range(0, prefix.Count);
         int two = Random.Range(0, middle.Count);
-        int three = Random.Range(0, middle.Count);
+        int three = Random.Range(0, suffix.Count);
         string pref = prefix[one];
         string mid =  middle[two];
         string suf =  suffix[three];
